da37a9d8103cb4e7411cd6864376ea80c15ae93c
mao <mxw1453@gmail.com>
2017-05-25 00:34:47 +0800

    增加Test1文件

---
+++ complete/src/test/java/hello/Test1.java
@@ -0,0 +1,22 @@
+/** 
+ * Project Name: complete 
+ * File Name: Test1.java 
+ * Package Name: hello 
+ * Date: 2017年5月25日上午12:34:30 
+ * Copyright (c) 2017, www.huizhaofang.com All Rights Reserved. 
+ * 
+ */  
+package hello;
+
+/** 
+ * ClassName: Test1
+ * date: 2017年5月25日 上午12:34:30
+ * Description: 
+ * 
+ * @author Mao-Pro 
+ * @version  
+ * @since JDK 1.8 
+ */
+public class Test1 {
+
+}
